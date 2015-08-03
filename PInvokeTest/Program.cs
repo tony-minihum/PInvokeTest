d40697c013ed16a4bc374daf0e6853c50d47012d
toshi_t <toshiharua.tanaka@jp.sony.com>
2015-08-03 17:05:38 +0900

    fix tab length

--- PInvokeTest/Program.cs
+++ PInvokeTest/Program.cs
@@ -6,47 +6,47 @@ using System.Runtime.InteropServices;
 
 namespace PInvokeTest
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-        print_line("Hello PInvoke!!!");
-        Console.WriteLine();
-
-        var buf = new StringBuilder(5);
-        var bufCanary = new StringBuilder(5);
-        get_string(buf, buf.Capacity);
-
-        Console.WriteLine("buf:" + buf + "[EOB] " + buf.Capacity);
-        Console.WriteLine("canary:" + bufCanary + "[EOB]" + bufCanary.Capacity);
-
-        var intBuf = new int[5];
-        var intBufCanary = new int[5];
-        takes_an_int_array(intBuf, intBuf.Length);
-
-        for (int i = 0; i < intBuf.Length; i++)
+        static void Main(string[] args)
         {
-            Console.WriteLine("intBuf:" + intBuf[i] + "[EOB] " + intBuf.Length);
-        }
-        for (int i = 0; i < intBufCanary.Length; i++)
-        {
-            Console.WriteLine("canary:" + intBufCanary[i] + "[EOB]" + intBufCanary.Length);
+            print_line("Hello PInvoke!!!");
+            Console.WriteLine();
+
+            var buf = new StringBuilder(5);
+            var bufCanary = new StringBuilder(5);
+            get_string(buf, buf.Capacity);
+
+            Console.WriteLine("buf:" + buf + "[EOB] " + buf.Capacity);
+            Console.WriteLine("canary:" + bufCanary + "[EOB]" + bufCanary.Capacity);
+
+            var intBuf = new int[5];
+            var intBufCanary = new int[5];
+            takes_an_int_array(intBuf, intBuf.Length);
+
+            for (int i = 0; i < intBuf.Length; i++)
+            {
+                Console.WriteLine("intBuf:" + intBuf[i] + "[EOB] " + intBuf.Length);
+            }
+            for (int i = 0; i < intBufCanary.Length; i++)
+            {
+                Console.WriteLine("canary:" + intBufCanary[i] + "[EOB]" + intBufCanary.Length);
+            }
+
+            Console.WriteLine("\n\nPress any key...");
+            Console.ReadKey();
         }
 
-        Console.WriteLine("\n\nPress any key...");
-        Console.ReadKey();
+        [DllImport("NativeLib.dll")]
+        private static extern void print_line(string str);
+
+        [DllImport("NativeLib.dll")]
+        private static extern void get_string(StringBuilder buf, int len);
+
+        [DllImport("NativeLib.dll")]
+        private static extern void takes_an_int_array(
+            [Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] int[] intArray,
+            int len
+            );
     }
-
-    [DllImport("NativeLib.dll")]
-    private static extern void print_line(string str);
-
-    [DllImport("NativeLib.dll")]
-    private static extern void get_string(StringBuilder buf, int len);
-
-    [DllImport("NativeLib.dll")]
-    private static extern void takes_an_int_array(
-        [Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex=1)] int[] intArray,
-        int len
-        );
-  }
 }
